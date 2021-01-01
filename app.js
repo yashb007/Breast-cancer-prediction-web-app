ca1ea509ee8ffee930417bbdfb59dec1bf8198d3
yashb007 <bansaly37@gmail.com>
2021-01-01 12:53:20 +0530

    another

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ app.use(cors())
 app.use(express.json())
  app.use('/',require('./user'))
 
-// if(process.env.NODE_ENV=="production"){
-//     app.use(Express.static('client/build'))
-//     const path = require('path')
-//     app.get('*',(req,rse)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if(process.env.NODE_ENV=="production"){
+    app.use(Express.static('client/build'))
+    const path = require('path')
+    app.get('*',(req,rse)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is started at ${PORT}`)
