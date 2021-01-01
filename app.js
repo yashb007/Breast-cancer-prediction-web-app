9b036e9f373816db0a5ecadf02c83774ccc20711
yashb007 <bansaly37@gmail.com>
2021-01-01 13:15:45 +0530

    another

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
- app.use('/',require('./user'))
+app.use('/',require('./user'))
 
-if(process.env.NODE_ENV=="production"){
-    app.use(Express.static('client/build'))
+ if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
     const path = require('path')
-    app.get('*',(req,rse)=>{
+    app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
